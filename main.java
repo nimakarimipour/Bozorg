3a12b1694cc124d893d3f9e6e06b29f62f53e627
Armin <gamemaking.ar@gmail.com>
2015-04-25 18:03:22 +0430

    first ch
    F dodange!!

--- main.java
+++ main.java
@@ -3,7 +3,7 @@ public class main{
     public static void main(String[] args){
 
         for(int i = 0; i < 1000000; ++i){
-            System.out.println("Dodange");
+            System.out.println7("jf Dodange");
         }
     }
 }
