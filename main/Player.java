d130eaa3e721e816e68e3d157372b4c548a54f92
Nima Karimipour <karimipour.nima@gmail.com>
2015-05-06 20:36:34 +0430

    Update Player.java
    Pay attention to packages

--- main/Player.java
+++ main/Player.java
@@ -1,4 +1,57 @@
-package main;
+package person;
 
-public class Player extends Person {
+import bozorg.common.GameObjectID;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+/**
+ * Created by nima on 5/6/15.
+ */
+public class Player {
+
+    playerType type;
+    Eye eye;
+    ArrayList<Fan> fans = new ArrayList<Fan>();
+    HashMap<GameObjectID, Fan> thrownFanHashMap = new HashMap<GameObjectID, Fan>();
+
+    public Player(int id){
+
+        //Kholase
+
+    }
+
+    public void move (int direction){
+
+    }
+
+    public void attack (int direction){
+
+    }
+
+    public GameObjectID throwFan(){
+        //TODO addException instead null
+        if(fans.size() != 0) {
+            Fan thrownFan = fans.get(0);
+            fans.remove(0);
+            GameObjectID gameObjectID = GameObjectID.create(Fan.class);
+            thrownFanHashMap.put(gameObjectID, thrownFan);
+            return gameObjectID;
+        }
+        else return null;
+    }
+
+    public void getGift(){}
+
+    public ArrayList<String> getVision(){
+        return eye.getVision();
+
+    }
+
+    public ArrayList<Player> getPlayersInVision(){
+        return null;
+    }
 }
+
+
+
